04ffb22f6a199bc7c765c7d90d75f8a286c3d45f
Santicm23 <santicastrom@outlook.com>
2023-02-15 11:10:40 -0500

    acceder comandos desde un map

--- main.cpp
+++ main.cpp
@@ -2,127 +2,55 @@
 #include<iostream>
 #include<string>
 #include<sstream>
+#include<map>
 
 using namespace std;
 
 
+void salir(string) {};
+
+void ayuda(string command) {
+    cout<<"cargar_comandos      \n"
+        <<"cargar_elementos     \n"
+        <<"agregar_movimiento   \n"
+        <<"agregar_analisis     \n"
+        <<"agregar_elemento     \n"
+        <<"guardar              \n"
+        <<"simular_comandos     \n"
+        <<"ubicar_elementos     \n"
+        <<"en_cuadrante         \n"
+        <<"crear_mapa           \n"
+        <<"ruta_mas_larga       \n";
+}
+
+void cargar_comandos(string command) {
+    string parametro1,parametro2;
+    stringstream partir(command);
+    partir>>parametro1>>parametro1;
+    cout<<"1."<<parametro1<<endl;
+}
+
 int main(){
-    string command;
-    do  {
+    string command, word;
+
+    map<string, void(*)(string)> commands;
+    commands.insert({"salir", salir});
+    commands.insert({"ayuda", ayuda});
+    commands.insert({"cargar_comandos", cargar_comandos});
+
+    do {
         cout<<"$ ";
-        getline(cin,command);
-        if (command == "") 
-        {
+        getline(cin, command);
 
-        }
-        else if (command.find("cargar_comandos")!=string::npos)
-        {
-            string parametro1,parametro2;
-            stringstream partir(command);
-            partir>>parametro1>>parametro1;
-            cout<<"1."<<parametro1<<endl;
-            
-        } 
-        else if(command == "cargar_elementos")
-        {
-            string a;
-        }
-        else if(command == "agregar_movimiento")
-        {
-            string a;
-        }
-        else if(command == "agregar_analisis")
-        {
-            string a;
-        }
-        else if(command == "agregar_elemento")
-        {
-            string a;
-        }
-        else if(command == "guardar")
-        {
-            string a;
-        }
-        else if(command == "simular_comandos")
-        {
-            string a;
-        }
-        else if(command == "ubicar_elementos")
-        {
-            string a;
-        }
-        else if(command == "en_cuadrante")
-        {
-            string a;
-        }
-        else if(command == "crear_mapa")
-        {
-            
-        }
-        else if(command == "ruta_mas_larga")
-        {
-            
-        }
-        else if (command == "ayuda") 
-        {
-            cout<<"cargar_comandos      "<<"\n"
-                <<"cargar_elementos     "<<"\n"
-                <<"agregar_movimiento   "<<"\n"
-                <<"agregar_analisis     "<<"\n"
-                <<"agregar_elemento     "<<"\n"
-                <<"guardar              "<<"\n"
-                <<"simular_comandos     "<<"\n"
-                <<"ubicar_elementos     "<<"\n"
-                <<"en_cuadrante         "<<"\n"
-                <<"crear_mapa           "<<"\n"
-                <<"ruta_mas_larga       "<<"\n";
-        }
-        else if (command == "ayuda cargar_comandos")
-        {
+        stringstream stream(command);
+        getline(stream, word, ' ');
 
-        } 
-        else if(command == "ayuda cargar_elementos")
-        {
-
-        }
-        else if(command == "ayuda agregar_movimiento")
-        {
-            
-        }
-        else if(command == "ayuda agregar_analisis")
-        {
-            
-        }
-        else if(command == "ayuda agregar_elemento")
-        {
-            
-        }
-        else if(command == "ayuda guardar")
-        {
-            
-        }
-        else if(command == "ayuda simular_comandos")
-        {
-            
-        }
-        else if(command == "ayuda ubicar_elementos")
-        {
-            
-        }
-        else if(command == "ayuda en_cuadrante")
-        {
-            
-        }
-        else if(command == "ayuda crear_mapa")
-        {
-            
-        }
-        else if(command == "ayuda ruta_mas_larga")
-        {
-            
-        }
-        else{
-            cout<<"Aprende a escribir\n";
+        if (command == "") {
+            continue;
+        } else if (commands.count(word) > 0){
+                commands[word](command);
+        } else {
+            cerr << "El comando no existe" << '\n';
         }
             
     }while (command!="salir");
