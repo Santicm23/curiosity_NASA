78d5a91eb7590c2cb303f1e0f90c580a0bbb19a6
Santicm23 <santicastrom@outlook.com>
2023-02-15 20:22:22 -0500

    corrección bug jiji

--- main.cpp
+++ main.cpp
@@ -11,18 +11,6 @@ using namespace std;
 map<string, string> commandHelps;
 map<string, void(*)(vector<string>)> commands;
 
-void fillMaps(map<string, string> &descMap, map<string, void(*)(vector<string>)>&exeMap) {
-    descMap.insert({"cargar_comandos", "c"});
-    exeMap.insert({"cargar_comandos", cargar_comandos});
-
-    descMap.insert({"salir", "a"});
-    exeMap.insert({"salir", salir});
-
-    descMap.insert({"ayuda", "b"});
-    exeMap.insert({"ayuda", ayuda});
-
-}
-
 void salir(vector<string>) {}
 
 void cargar_comandos(vector<string> args) {}
@@ -44,6 +32,27 @@ void ayuda(vector<string> args) {
     
 }
 
+void fillMaps(map<string, string> &descMap, map<string, void(*)(vector<string>)>&exeMap) {
+    // command name, command description
+    // command name, command callback function
+
+    descMap.insert({"cargar_comandos",
+        "Carga en memoria los comandos de desplazamiento contenidos en el archivo identificado por nombre_archivo,\n"
+        "es decir, utiliza adecuadamente las estructuras lineales para cargar la información de los comandos en memoria.\n"
+        "Si dentro de la misma sesión de trabajo ya se han cargado otros archivos de comandos (usando el comando\n"
+        "cargar_comandos), la información debe sobreescribirse en memoria, es decir, no se deben combinar informaciones\n"
+        "de comandos de diferentes archivos."
+    });
+    exeMap.insert({"cargar_comandos", cargar_comandos});
+
+    descMap.insert({"salir", "a"});
+    exeMap.insert({"salir", salir});
+
+    descMap.insert({"ayuda", "b"});
+    exeMap.insert({"ayuda", ayuda});
+
+}
+
 int main(){
     string commandLine, command, word;
 
@@ -51,7 +60,7 @@ int main(){
     fillMaps(commandHelps, commands);
 
     do {
-        vector<string> inputCommand;
+        vector<string> args;
 
         cout<<"$ ";
         getline(cin, commandLine);
@@ -61,13 +70,13 @@ int main(){
         getline(stream, command, ' ');
 
         while(getline(stream, word, ' ')){
-            inputCommand.push_back(word);
+            args.push_back(word);
         }
 
         if (commandLine == "") {
             continue;
         } else if (commands.count(command) > 0){
-            commands[command](inputCommand);
+            commands[command](args);
         } else {
             cout << "El comando '"<< command <<"' no existe" << '\n';
         }
